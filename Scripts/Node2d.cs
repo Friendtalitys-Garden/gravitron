4b602202e58e0a8fcf4e74bd5bee145f2c495587
Till <till.bruegmann@icloud.com>
2025-06-02 10:42:17 +0200

    animated circle

--- Scripts/Node2d.cs
+++ Scripts/Node2d.cs
@@ -3,14 +3,29 @@ using System;
 
 public partial class Node2d : Node2D
 {
-	 public override void _Ready()
-	{
-		// This ensures the node is redrawn when ready
+    private Vector2 _circlePosition = new Vector2(0, 150); // Start position
+    private float _speed = 100f; // Pixels per second
 
-	}
-
-	public override void _Draw()
+	public override void _Ready()
 	{
-		DrawCircle(new Vector2(200, 150), 50, new Color(1, 0, 0));
-	}
+		// Trigger initial draw
+		QueueRedraw();
+    }
+
+	public override void _Process(double delta)
+	{
+		// Move circle to the right
+		_circlePosition.X += (float)(delta * _speed);
+
+		// Redraw every frame
+		QueueRedraw();
+    }
+
+    public override void _Draw()
+    {
+        float radius = 30;
+        Color color = new Color(1, 0, 0); // Red
+
+        DrawCircle(_circlePosition, radius, color);
+    }
 }
