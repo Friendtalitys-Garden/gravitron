26e5291fbeccae54b930fe0001a81f1c599170b9
Lukas Rischer <114649647+sudo-lukas@users.noreply.github.com>
2025-06-05 15:30:32 +0200

    Update Node2d.cs

--- Scripts/Node2d.cs
+++ Scripts/Node2d.cs
@@ -3,14 +3,14 @@ using System;
 
 public partial class Node2d : Node2D
 {
-    private Vector2 _circlePosition = new Vector2(0, 150); // Start position
-    private float _speed = 100f; // Pixels per second
+	private Vector2 _circlePosition = new Vector2(0, 150); // Start position
+	private float _speed = 100f; // Pixels per second
 
 	public override void _Ready()
 	{
 		// Trigger initial draw
 		QueueRedraw();
-    }
+	}
 
 	public override void _Process(double delta)
 	{
@@ -19,13 +19,13 @@ public partial class Node2d : Node2D
 
 		// Redraw every frame
 		QueueRedraw();
-    }
+	}
 
-    public override void _Draw()
-    {
-        float radius = 30;
-        Color color = new Color(1, 0, 0); // Red
+	public override void _Draw()
+	{
+		float radius = 30;
+		Color color = new Color(1, 0, 0); // Red
 
-        DrawCircle(_circlePosition, radius, color);
-    }
+		DrawCircle(_circlePosition, radius, color);
+	}
 }
